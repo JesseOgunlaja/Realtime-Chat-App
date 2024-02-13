142680af1d9fec4c349164e3461e29e4ce58c460
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2024-02-13 13:53:43 +0000

    Attempt to fix websockets

--- utils/websocketsServer.ts
+++ utils/websocketsServer.ts
@@ -1,14 +1,14 @@
 import { io } from "socket.io-client";
 
-const socket = io(process.env.WEBSOCKET_URL, {
-  auth: {
-    password: process.env.WEBSOCKET_KEY,
-  },
-  query: {
-    test: "hi",
-  },
-});
-
 export function trigger(id: string, event: string, data: unknown) {
+  const socket = io(process.env.WEBSOCKET_URL, {
+    auth: {
+      password: process.env.WEBSOCKET_KEY,
+    },
+    query: {
+      test: "hi",
+    },
+  });
+
   socket.emit(process.env.WEBSOCKET_KEY, id, event, data);
 }
