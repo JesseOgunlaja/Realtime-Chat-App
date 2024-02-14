61cf2351129619a69d170288929be4a36c22faca
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2024-02-14 21:26:05 +0000

    Try to fix websockets

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
   reactStrictMode: false,
   transpilePackages: ["crypto-js"],
-  webpack: (config, { isServer }) => {
-    isServer && (config.externals = [...config.externals, "socket.io-client"]);
-    return config;
-  },
   images: {
     remotePatterns: [
       {
